c507596b92b942961ae7c89551529c0528b111ce
Chris <christofferhaj@gmail.com>
2020-09-02 17:39:21 +0200

    Updated collide.rb to follow style conventions

--- collide.rb
+++ collide.rb
@@ -1,22 +1,42 @@
-class Collide
-    def check_collision(snake, apples)
-        retn = [false, false]
-        index = 0
-        snake.each do |part|
-            if index > 0
-                if (snake.first[0] == part[0]) && (snake.first[1] == part[1])
-                    retn = ['game over', part]
-                    break
-                end
-            end
-            index += 1
-        end
+# frozen_string_literal: true
 
-        apples.each do |apple|
-            if (snake.first[0] == apple[0]) && (snake.first[1] == apple[1])
-                retn = ['grow', apple]
-            end
-        end
-        retn
+# Collide class
+# This class is used to check the collision between snake and apples
+# and snake itself
+class Collide
+  attr_accessor :retn
+
+  def initialize
+    @retn = [false, false]
+  end
+
+  def check_collision(snake, apples)
+    reset_retn
+    check_snake(snake)
+    check_food(snake, apples)
+    @retn
+  end
+
+  def check_food(snake, apples)
+    apples.each do |apple|
+      if (snake.first[0] == apple[0]) && (snake.first[1] == apple[1]) then self.retn = ['grow', apple] end
     end
+  end
+
+  def check_snake(snake)
+    index = 0
+    snake.each do |part|
+      if index.positive?
+        if (snake.first[0] == part[0]) && (snake.first[1] == part[1])
+          @retn = ['game over', part]
+          break
+        end
+      end
+      index += 1
+    end
+  end
+
+  def reset_retn
+    self.retn = [false, false]
+  end
 end
