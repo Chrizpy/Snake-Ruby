990f9f051b0a460c7eff14d43bad48600add402c
Chris <christofferhaj@gmail.com>
2020-09-02 17:58:35 +0200

    Update grid.rb to follow style conventions
    I kept the for leep the same way for readability.

--- grid.rb
+++ grid.rb
@@ -1,27 +1,33 @@
+# frozen_string_literal: true
+
+# Grid class
+# This class is responsible for creating the grid in which the playground
+# is used for playing the game.
 class Grid
-    attr_reader :background, :size, :width, :height
+  attr_reader :background, :size, :width, :height, :grid
 
-    def initialize(width, height)
-        @background = 'blue'
-        @size = 20
-        @width = width / @size
-        @height = height / @size
-        @grid = []
+  def initialize(width, height)
+    @background = 'blue'
+    @size = 20
+    @width = width / @size
+    @height = height / @size
+    @grid = []
 
-        for y in 0..@height
-            for x in 0..@width
-                @grid.push([x, y])
-            end
-        end
+    for y in 0..self.height
+      for x in 0..self.width
+        grid.push([x, y])
+      end
     end
-    def draw
-        @grid.each do |part|
-            Square.new(
-                x: part[0] * @size,
-                y: part[1] * @size,
-                color: @background,
-                size: @size - 1
-                )
-        end
+  end
+
+  def draw
+    grid.each do |part|
+      Square.new(
+        x: part[0] * size,
+        y: part[1] * size,
+        color: background,
+        size: size - 1
+      )
     end
+  end
 end
