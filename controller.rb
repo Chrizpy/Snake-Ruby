077425ee8aac8bf4d67406311705057c590dfb95
Chris <christofferhaj@gmail.com>
2020-09-02 17:50:04 +0200

    Update controller.rb to follow style conventions
    I was not able to get it to completely follow the conventions, check_input is apperently too complex?

--- controller.rb
+++ controller.rb
@@ -1,36 +1,32 @@
-class Controller
-    attr_accessor :direction, :play
+# frozen_string_literal: true
 
-    def initialize
-        @direction = 'left'
-        @@last = @direction
-        @play = false
+# Controller class
+# This class is responsible for the maneuvering through out the game.
+class Controller
+  attr_accessor :direction, :play, :last
+
+  def initialize
+    @direction = 'left'
+    @last = @direction
+    @play = false
+  end
+
+  def check_input
+    Window.on :key_down do |event|
+      case event.key
+      when 'left'
+        if last != 'right' then self.direction = 'left' end
+      when 'right'
+        if last != 'left' then self.direction = 'right' end
+      when 'up'
+        if last != 'down' then self.direction = 'up' end
+      when 'down'
+        if last != 'up' then self.direction = 'down' end
+      when 'p' then self.play = true end
     end
-    def check_input
-        Window.on :key_down do |event|
-            case event.key
-            when 'left'
-                if @@last != 'right'
-                    @direction = 'left'
-                end
-            when 'right'
-                if @@last != 'left'
-                    @direction = 'right'
-                end
-            when 'up'
-                if @@last != 'down'
-                    @direction = 'up'
-                end
-            when 'down'
-                if @@last != 'up'
-                    @direction = 'down'
-                end
-            when 'p'
-                @play = true
-            end
-        end
-    end
-    def last_input
-        @@last = @direction
-    end
+  end
+
+  def last_input
+    self.last = direction
+  end
 end
